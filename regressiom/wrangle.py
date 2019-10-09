2f449b29dbd7ed75a6c04c159ddff716c0dea5d1
Adam H <mail4atom2k@gmail.com>
2019-10-09 14:54:05 -0500

    added steps to wrangle file

--- regressiom/wrangle.py
+++ regressiom/wrangle.py
@@ -1,6 +1,55 @@
 # wrangle.py  
 # file for all REGRESSION session
 
+
+import warnings
+warnings.filterwarnings("ignore")
+
+import pandas as pd
+%matplotlib inline
+import matplotlib.pyplot as plt
+import seaborn as sns
+import numpy as np
+import env
+
+from scipy import stats
+from math import sqrt
+
+def get_db_url(db):
+    return f'mysql+pymysql://{env.user}:{env.password}@{env.host}/{db}'
+
+url = get_db_url('telco_churn')
+
+query = '''
+SELECT customer_id, monthly_charges, tenure, total_charges
+FROM customers
+WHERE contract_type_id = 3;
+'''
+
+telco_churn = pd.read_sql(query, url)
+
+telco_churn.head()
+
+print(telco_churn.dtypes)
+
+telco_churn.total_charges.replace(r'^\s*$', np.nan, regex=True, inplace=True)
+
+print(telco_churn.info())
+
+telco_churn['total_charges'] = pd.to_numeric(telco_churn['total_charges'], errors='coerce')
+
+telco_churn.info()
+
+ # change of variable - incase a step goes wrong - original variable is still intact
+print("+" * 20)
+print("NOTE: variable change to 'telco_churn2")
+print("+" * 20)
+telco_churn2 = telco_churn.dropna()
+
+telco_churn2.info()
+
+
+
 #### AQUIRE and PREP exercises ####
 
 # 1 Acquire customer_id, monthly_charges, tenure, and total_charges
