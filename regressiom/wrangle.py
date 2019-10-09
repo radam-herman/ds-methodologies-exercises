3bed2a59686f2d61396a8635613aaad8824da17c
Adam H <mail4atom2k@gmail.com>
2019-10-09 12:18:51 -0500

    getting SQL DATA

--- regressiom/wrangle.py
+++ regressiom/wrangle.py
@@ -1,2 +1,14 @@
 # wrangle.py  
 # file for all REGRESSION session
+
+#### AQUIRE and PREP exercises ####
+
+# 1 Acquire customer_id, monthly_charges, tenure, and total_charges
+#  from telco_churn database for all customers with a 2 year contract.
+
+# 2 Walk through the steps above using your new dataframe. You may 
+# handle the missing values however you feel is appropriate.
+
+# 3 End with a python file wrangle.py that contains the function, 
+# wrangle_telco(), that will acquire the data and return 
+# a dataframe cleaned with no missing values.
