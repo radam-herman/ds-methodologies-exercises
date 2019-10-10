0dc793a653b39bac64ba70deee2a110671019699
Adam H <mail4atom2k@gmail.com>
2019-10-10 08:42:15 -0500

    added exercise file for SCALING

---
+++ regressiom/split_scale.py
@@ -0,0 +1,29 @@
+# REGRESSION - split_scale.py exercise
+
+# Each scaler function should create the object, 
+# fit and transform both train and test. 
+# They should return the scaler, train df scaled, 
+# test df scaled. Be sure your indices represent 
+# the original indices from train/test, as those represent 
+# the indices from the original dataframe. 
+# Be sure to set a random state where applicable for reproducibility!
+
+# 1 - split_my_data(X, y, train_pct)
+
+
+# 2 standard_scaler()
+
+
+# 3 scale_inverse()
+
+
+# 4 uniform_scaler()
+
+
+# 5 gaussian_scaler()
+
+
+# 6 min_max_scaler()
+
+
+# 7 iqr_robust_scaler()
