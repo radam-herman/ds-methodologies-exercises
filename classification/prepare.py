10e7abb2010c99c7eb44457c911d1e830301a52f
Adam H <mail4atom2k@gmail.com>
2019-10-23 15:56:55 -0500

    making PY files for exercises

--- classification/prepare.py
+++ classification/prepare.py
@@ -1,2 +1,26 @@
 # prepare.py created out of DATA PREP excercises
 
+# # Prepare Data
+
+# - drop columns
+# - fillna
+# - split (aka train_test_split into train/test)
+# - impute mean, mode, median: SimpleImputer
+# - integer encoding: LabelEncoder
+# - one hot encoding: OneHotEncoder
+# - scale
+
+import pandas as pd
+import numpy as np
+
+from sklearn.model_selection import train_test_split
+
+
+def split_data(df): # this function takes in a dataframe - spits out test and train
+    df = df.fillna(np.nan)
+    seed = 123
+    train_prct = .8
+    train, test = train_test_split(df, train_size=train_prct, random_state=seed)
+
+    return train, test
+
