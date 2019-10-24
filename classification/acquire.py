63591efffbc2420f153224aa1fead38ace8c4882
Adam H <mail4atom2k@gmail.com>
2019-10-24 10:03:28 -0500

    added IRIS fm MML

--- classification/acquire.py
+++ classification/acquire.py
@@ -5,13 +5,16 @@ warnings.filterwarnings("ignore")
 import pandas as pd
 import env
 
+#from sklearn.datasets import load_iris # for iris data
+from sklearn import datasets
+
 def get_connection(db, user=env.user, host=env.host, password=env.password):
     return f'mysql+pymysql://{user}:{password}@{host}/{db}'
 
 def get_titanic_data():
     return pd.read_sql('SELECT * FROM passengers', get_connection('titanic_db'))
 
-def get_iris_data():
+def get_iris_data():  # THIS KEPT GETTING A "Exception: Data must be 1-dimensional"
 
     query = '''
 SELECT * 
@@ -19,3 +22,7 @@ FROM species as s
 JOIN measurements as m ON s.species_id = m.species_id
     '''
     return pd.read_sql(query, get_connection('iris_db'))
+
+def get_iris_data_sklearn_edition():
+    iris = datasets.load_iris()
+    return iris
--- classification/prepare.py
+++ classification/prepare.py
@@ -19,7 +19,7 @@ from sklearn.model_selection import train_test_split
 def split_data(df): # this function takes in a dataframe - spits out test and train
     df = df.fillna(np.nan)
     seed = 123
-    train_prct = .8
+    train_prct = .7
     train, test = train_test_split(df, train_size=train_prct, random_state=seed)
 
     return train, test
